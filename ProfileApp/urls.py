c7353bc1c9007be65b7a010f23e711cd9fddf812
banthat <banthat.up@rmuit.ac.th>
2023-01-17 10:51:29 +0700

    first commit 17/1/2023

---
+++ ProfileApp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from ProfileApp import views
+
+urlpatterns = [
+    path('', views.homepage, name='homepage'),
+    path('hello', views.helloworld, name='hello',),
+    path('firstpage', views.firstPage, name='firstpage'),
+    path('secondpage', views.secondpage, name='secondpage'),
+    path('thirdpage', views.thirdpage, name='thirdpage'),
+    path('hnypage', views.hnypage, name='hnypage'),
+]
---
+++ ProfileApp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, HttpResponse
+
+# Create your views here.
+def helloworld(request):
+    return HttpResponse("<H1>Hello World, This is my First Web </H1>"
+                        "<H2>I Love Girls' Generation, It's My Life</H2>")
+
+def firstPage(request):
+    return render(request, 'FirstPage.html')
+
+def secondpage(request):
+    return render(request, 'SecondPage.html')
+
+def thirdpage(request):
+    return render(request, 'thirdpage.html')
+
+def hnypage(request):
+    return render(request, 'hnypage.html')
+
+def homepage(request):
+    return render(request, 'HomePage.html')
